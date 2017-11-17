61942c0fb935d1c8029e523e0d88f4b81f85b1fe
RvkACEaMlh FcJVVeAncA <automate11@wavemaker.com>
2017-11-17 12:06:14 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_SOSOjzsgiW",
+  "homePage" : "Main",
+  "name" : "Auto_SOSOjzsgiW",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
